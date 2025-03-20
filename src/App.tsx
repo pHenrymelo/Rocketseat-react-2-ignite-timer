58426340f1bce876a6d808415a62fd0f270c6d47
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-03-20 20:03:36 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>aoba</h1>
+    </ThemeProvider>
+  )
+}
+
