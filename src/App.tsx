8c1a8a204b321ef9bb9ae03d7e2840b3227cd27c
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-03-21 14:22:55 -0300

    feature: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>aoba</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "../assets/Logo";
+
+
+export function Header() {
+    return (
+        <div>
+            <Logo />
+            <span>
+
+            </span>
+        </div>
+    )
+}
